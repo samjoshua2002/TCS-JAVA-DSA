9f99e30d996c14a817cd69f6929cdf9a52819a3b
Sam Joshua <samjoshuacs2002@gmail.com>
2024-07-15 16:41:19 +0530

    done

--- youtube/overloaded.java
+++ youtube/overloaded.java
@@ -1,10 +1,27 @@
 package youtube;
 
-import java.util.ArrayList;
+
 
 public class overloaded {
+
+
+static int add(int a, int b) {
+System.out.println("This is overloaded method #1");
+return a + b;
+}
+
+static int add(int a, int b, int c) {
+System.out.println("This is overloaded method #2");
+return a + b + c;
+}
+static int add(int a, int b, int c, int d) {
+System.out.println("This is overloaded method #3");
+return a + b + c + d;
+
+}
     public static void main(String[] args) {
-        
+        int x = add(4, 7, 8, 9);
+        System.out.println(x);
          
     }
 }
