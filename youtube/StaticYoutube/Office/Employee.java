fdbde7363210a04dde4bd106c9ade96d57eb2205
Sam Joshua P <samjoshuacs2002@gmail.com>
2024-08-18 20:21:21 +0530

    done

--- youtube/StaticYoutube/Office/Employee.java
+++ youtube/StaticYoutube/Office/Employee.java
@@ -4,7 +4,19 @@ public class Employee {
     int id;
     String name;
     int salary;
-    String ceo;
+
+    
+    static String ceo;
+
+    public Employee() {
+        id = 0;
+        name = "none";
+        salary = 0;
+    }
+    static{
+        ceo="none";
+    }
+    
     public void show(){
         System.out.println(id+" : "+name+" : "+salary+" : "+ceo);
     }
--- youtube/StaticYoutube/Office/company.java
+++ youtube/StaticYoutube/Office/company.java
@@ -3,18 +3,21 @@ package youtube.StaticYoutube.Office;
 public class company {
     public static void main(String[] args) {
         Employee sam = new Employee();
-        sam.id=4130;
-        sam.name="Sam Joshua";
-        sam.salary=70000;
-        sam.ceo="Mahesh";
+        // sam.id=4130;
+        // sam.name="Sam Joshua";
+        // sam.salary=70000;
+        // Employee.ceo="Mahesh";
         Employee sanjay = new Employee();
-        sanjay.id=4130;
-        sanjay.name="Sam Joshua";
-        sanjay.salary=70000;
-        sanjay.ceo="Mahesh";
+        // sanjay.id=4130;
+        // sanjay.name="Sanjay Kumar";
+        // sanjay.salary=75000;
+        // Employee.ceo="Mahesh";
+        // Employee.ceo="Shakeen";
+        Employee prem =new Employee();
 
         sam.show();
         sanjay.show();
+        prem.show();
         
         
     }
