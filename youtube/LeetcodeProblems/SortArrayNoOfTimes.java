f5e7ec06d042dddac1deccb848ef299f8f55c057
Sam Joshua P <samjoshuacs2002@gmail.com>
2024-08-18 20:57:18 +0530

    done

--- youtube/LeetcodeProblems/SortArrayNoOfTimes.java
+++ youtube/LeetcodeProblems/SortArrayNoOfTimes.java
@@ -3,33 +3,45 @@ package youtube.LeetcodeProblems;
 import java.util.Scanner;
 
 public class SortArrayNoOfTimes {
-    public static void reverse( int[] num , int start ,int end){
-        while (start<end) {
-            int temp=num[start];
-            num[start]=num[end];
-            num[end]=temp;
+    public static void reverse(int[] num, int start, int end) {
+        while (start < end) {
+            int temp = num[start];
+            num[start] = num[end];
+            num[end] = temp;
             start++;
             end--;
-
-            
         }
-        for (int i : num) {
-            System.out.print(i+ " ");
-            
-        }
-        
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the list");
+        System.out.println("Enter the list of numbers separated by spaces:");
         String result = scanner.nextLine();
         String[] array = result.split(" ");
         int[] arr = new int[array.length];
         for (int i = 0; i < array.length; i++) {
             arr[i] = Integer.parseInt(array[i]);
         }
+
+        System.out.println("Enter number of times the values need to be rotated:");
+        int k = scanner.nextInt();
+
+        
+        k = k % arr.length;
+
         scanner.close();
-        reverse( arr ,0 , arr.length-1);
+
+        
+        reverse(arr, 0, arr.length - 1);
+        
+        reverse(arr, 0, k - 1);
+        
+        reverse(arr, k, arr.length - 1);
+
+        
+        System.out.println("The rotated array is:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
-    
 }
