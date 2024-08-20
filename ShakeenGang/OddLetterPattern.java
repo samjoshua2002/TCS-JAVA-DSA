690c3cc22175c2734f5132931b606dab0d1be1d6
Sam Joshua P <samjoshuacs2002@gmail.com>
2024-08-20 17:27:41 +0530

    ayooooooooooooo

--- ShakeenGang/OddLetterPattern.java
+++ ShakeenGang/OddLetterPattern.java
@@ -2,32 +2,35 @@ package ShakeenGang;
 
 public class OddLetterPattern {
     public static void main(String[] args) {
-        String word = "PROGRAM";
-        int length = word.length();
-        int mid = length / 2;
-
-       
-        for (int i = 0; i <= mid; i++) {
-            for (int j = 0; j < length; j++) {
-                if (j == i || j == length - i - 1) {
+        String word="PROGRAM";
+        int Length=word.length();
+        int mid= Length/2;
+        for (int i = 0; i <=mid; i++) {
+            for( int j =0 ; j< Length; j++){
+                if (j==i||j==Length-i-1) {
                     System.out.print(word.charAt(j));
-                } else {
+                    
+                }else{
+
                     System.out.print(" ");
                 }
             }
             System.out.println();
+            
         }
-
-        
-        for (int i = mid - 1; i >= 0; i--) {
-            for (int j = 0; j < length; j++) {
-                if (j == i || j == length - i - 1) {
+        for (int i = mid-1; i >=0; i--) {
+            for( int j =0 ; j< Length; j++){
+                if (j==i||j==Length-i-1) {
                     System.out.print(word.charAt(j));
-                } else {
+                    
+                }else{
+
                     System.out.print(" ");
                 }
             }
             System.out.println();
+            
         }
+
     }
 }
---
+++ ShakeenGang/WeightCalculator.java
@@ -0,0 +1,48 @@
+package ShakeenGang;
+
+import java.util.*;
+
+public class WeightCalculator {
+    
+    // Method to calculate the weight of a given number
+    public static int calculateWeight(int num) {
+        int weight = 0;
+
+        // Check if the number is a perfect square
+        if (Math.sqrt(num) == (int)Math.sqrt(num)) {
+            weight += 5;
+        }
+
+        // Check if the number is a multiple of 4 and divisible by 6
+        if (num % 4 == 0 && num % 6 == 0) {
+            weight += 4;
+        }
+
+        // Check if the number is even
+        if (num % 2 == 0) {
+            weight += 3;
+        }
+
+        return weight;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 36, 54, 89, 12};
+        List<int[]> weightedNumbers = new ArrayList<>();
+
+        // Calculate the weight for each number and store in the list
+        for (int num : numbers) {
+            int weight = calculateWeight(num);
+            weightedNumbers.add(new int[]{num, weight});
+        }
+
+        // Sort the list based on the weight (index 1 of the array)
+        weightedNumbers.sort(Comparator.comparingInt(a -> a[1]));
+
+        // Print the numbers and their weights
+        for (int[] entry : weightedNumbers) {
+            System.out.println("<" + entry[0] + ", " + entry[1] + ">");
+        }
+    }
+}
+
