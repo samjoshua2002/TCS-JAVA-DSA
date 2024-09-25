99e2dbd0f6a8fec5b1a3c33122e865a1513de755
Sam Joshua P <samjoshuacs2002@gmail.com>
2024-09-25 10:32:58 +0530

    jskdfahkjf

---
+++ greensjava/demo.java
@@ -0,0 +1,21 @@
+package greensjava;
+
+public class demo {
+	public void StudentName() {
+		System.out.println("this is student name :Sam Joshua");
+		
+
+	}
+	
+	private void age() {
+		
+		System.out.println("THIS IS STUDENT AGE :22");
+
+	}
+	public static void main(String[] args) {
+		demo obj = new demo();
+		obj.StudentName();
+		obj.age();
+	}
+
+}
