5f82443a6c7e06fa07d21b3ed18bb77bba4c515d
Sam Joshua P <samjoshuacs2002@gmail.com>
2025-02-05 11:08:49 +0530

    changes

--- ConceptsFrRevision/section-05-arrays/src/main/java/ComputeGradeAverageDemo.java
+++ ConceptsFrRevision/section-05-arrays/src/main/java/ComputeGradeAverageDemo.java
@@ -4,37 +4,36 @@ public class ComputeGradeAverageDemo {
 
     public static void main(String[] args) {
 
-        // prompt the user for how many grades
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("How many grades will you enter? ");
-        int numGrades = scanner.nextInt();
-
-        System.out.println();
-
-        // initialize the array based on the number of grades
-        double[] userInputGrades = new double[numGrades];
-
-        // read each grade and assign to an array element
-        for (int i=0; i < userInputGrades.length; i++) {
-            System.out.print("Enter grade number " + (i+1) + ": ");
-            userInputGrades[i] = scanner.nextDouble();
+        try ( // prompt the user for how many grades
+                Scanner scanner = new Scanner(System.in)) {
+            System.out.print("How many grades will you enter? ");
+            int numGrades = scanner.nextInt();
+            
+            System.out.println();
+            
+            // initialize the array based on the number of grades
+            double[] userInputGrades = new double[numGrades];
+            
+            // read each grade and assign to an array element
+            for (int i=0; i < userInputGrades.length; i++) {
+                System.out.print("Enter grade number " + (i+1) + ": ");
+                userInputGrades[i] = scanner.nextDouble();
+            }
+            
+            System.out.println();
+            
+            // print out the array elements
+            for (double temp : userInputGrades) {
+                System.out.println(temp);
+            }
+            
+            System.out.println();
+            
+            // compute average
+            double gradeAverage = computeGradeAverage(userInputGrades);
+            
+            System.out.println("Grade average: " + gradeAverage);
         }
-
-        System.out.println();
-
-        // print out the array elements
-        for (double temp : userInputGrades) {
-            System.out.println(temp);
-        }
-
-        System.out.println();
-
-        // compute average
-        double gradeAverage = computeGradeAverage(userInputGrades);
-
-        System.out.println("Grade average: " + gradeAverage);
-
-        scanner.close();
 
     }
 
